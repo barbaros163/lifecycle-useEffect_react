ef6e9ee63f245b433d2aa4b968890d59ab32fe05
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-11 15:34:45 +0100

    didmount added

--- rc08_lifecycle-useEffect/src/App.js
+++ rc08_lifecycle-useEffect/src/App.js
@@ -1,10 +1,10 @@
 // import { useState } from "react";
-// import LifeCycleMethods from "./components/LifeCycleMethods";
+import LifeCycleMethods from "./components/LifeCycleMethods";
 
 function App() {
   return (
     <div className="container text-center mt-4">
-      <h1>hey ya</h1>
+      <LifeCycleMethods />
     </div>
   );
 }
