a49e22c7f6263c65f914413320ebbf71f42f9818
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-12 11:21:18 +0100

    user is done

--- rc08_lifecycle-useEffect/src/components/User.jsx
+++ rc08_lifecycle-useEffect/src/components/User.jsx
@@ -1,10 +1,34 @@
+import { useState, useEffect } from "react";
+
 const User = () => {
+  const [user, setUser] = useState("");
+
+  const getUser = () => {
+    fetch("https://randomuser.me/api")
+      .then((res) => res.json())
+      .then((data) => setUser(data.results[0]));
+  };
+
+  useEffect(() => {
+    //?componentDidMount
+    getUser();
+  }, []);
+
+  const { name, dob, email, picture } = user;
+
   return (
     <div>
-      <h1>NAME</h1>
-      <img src="" alt="" />
-      <p>email</p>
-      <h5>DOB</h5>
+      <h1>
+        {/* Optional Chaining */}
+        {/* name?.first?.second */}
+        {name?.first} {name?.last}{" "}
+      </h1>
+      <img className="rounded-circle" src={picture?.large} alt="img" />
+      <p>{email}</p>
+      <h5>{new Date(dob?.date).toLocaleDateString()}</h5>
+      <button className="btn btn-danger" onClick={getUser}>
+        Get User
+      </button>
     </div>
   );
 };
